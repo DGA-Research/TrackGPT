08f6ee5404c125d523443b8a3657ca416ee5a907
DGA-Research <dgaresearchcoding@gmail.com>
2025-06-23 10:30:43 -0400

    Update output.py

--- output.py
+++ output.py
@@ -587,10 +587,10 @@ def generate_html_report_bullets(
                   if not safe_url.startswith(('http://', 'https://')): safe_url = 'http://' + safe_url
                   # Use the already escaped date for the link text
                   safe_link_text = safe_formatted_date_mdy
-                  citation = f'[{safe_source}, <a href="{safe_url}" target="_blank" rel="noopener noreferrer"><em>{safe_link_text}</em></a>] ({file_type})'
+                  citation = f'[{safe_source}, <a href="{safe_url}" target="_blank" rel="noopener noreferrer"><em>{safe_link_text}</em></a>] ({type_input})'
              else:
                   # Use already escaped components
-                  citation = f'[{safe_source}, {safe_formatted_date_mdy}] (({file_type})'
+                  citation = f'[{safe_source}, {safe_formatted_date_mdy}] (({type_input})'
 
              # Escape the main headline and body text
              # Apply strict title case to the headline before escaping
