e9c9973efca7e1567aaddb3a7c16a6563e95aa28
DGA-Research <dgaresearchcoding@gmail.com>
2025-08-06 12:32:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ if check_password():
 
                 # st.session_state.speaker_list = ["Speaker A (Troy)", "Speaker B (Karrin Taylor Robson)"]
                 
-                pattern = r'Speaker\s+([A-Z]):\s+([\w\s]+)'
+                pattern = r'Speaker B:\s+([\s\S]*?)(?=\nSpeaker [A-Z]:|$)'
                 
                 # Make list of edited speakers
                 matches = re.findall(pattern, edited_speaker)
