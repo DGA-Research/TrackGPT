1ec8603f6c0c054959ce6b4f53868cf508ecd782
DGA-Research <dgaresearchcoding@gmail.com>
2025-08-06 13:14:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ if check_password():
                     unique_speakers1 = set()
                     
                     # Format each speaker and add to set (duplicates automatically ignored)
-                    for speaker_id, name in matches:
+                    for name in matches:
                         formatted_speaker = f"{speaker_id} ({name})"
                         unique_speakers.add(formatted_speaker)
                     
